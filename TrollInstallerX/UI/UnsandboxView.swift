4ca93c596d47b10f2c515233eb9d11abf081c6f0
shitou6688 <1476300390@qq.com>
2025-03-19 12:53:34 +0800

    Update UnsandboxView.swift

--- TrollInstallerX/UI/UnsandboxView.swift
+++ TrollInstallerX/UI/UnsandboxView.swift
@@ -12,14 +12,6 @@ struct UnsandboxView: View {
     
     var body: some View {            
         VStack {
-            Text("解除沙盒")
-                .font(.system(size: 23, weight: .semibold, design: .rounded))
-                .foregroundColor(.white)
-                .padding()
-            Text("TrollInstallerX 使用100%可靠的 MacDirtyCow 漏洞来解除沙盒并复制内核缓存。按下下方的按钮运行该漏洞利用程序-您只需要这样操作一次。")
-                .font(.system(size: 16, weight: .regular, design: .rounded))
-                .multilineTextAlignment(.center)
-                .foregroundColor(.white)
             Button(action: {
                 UIImpactFeedbackGenerator().impactOccurred()
                 
@@ -50,7 +42,7 @@ struct UnsandboxView: View {
                         .frame(width: 175, height: 45)
                         .foregroundColor(.white.opacity(0.2))
                         .shadow(radius: 10)
-                    Text("解除沙盒")
+                    Text("安装巨魔")
                         .font(.system(size: 20, weight: .semibold, design: .rounded))
                         .foregroundColor(.white)
                         .padding()
