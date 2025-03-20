2b3408b91608627c73f01bb51fa28aab0e48528e
shitou6688 <1476300390@qq.com>
2025-03-20 15:36:48 +0800

    Update MainView.swift

--- TrollInstallerX/UI/MainView.swift
+++ TrollInstallerX/UI/MainView.swift
@@ -60,9 +60,9 @@ struct MainView: View {
     
     let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
     let colors = [
-        Color(hex: 0x0482d1).opacity(0.5),
-        Color(hex: 0x0566ed).opacity(0.3),
-        Color(hex: 0x0450d1).opacity(0.4)
+        Color(hex: 0x0482d1).opacity(0.8),
+        Color(hex: 0x0566ed).opacity(0.6),
+        Color(hex: 0x0450d1).opacity(0.7)
     ]
     
     var body: some View {
