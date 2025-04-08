eaa66b58234470f5676a0d7286738b232b988b59
shitou6688 <1476300390@qq.com>
2025-04-09 01:18:47 +0800

    Update Installation.swift

--- TrollInstallerX/Installer/Installation.swift
+++ TrollInstallerX/Installer/Installation.swift
@@ -20,9 +20,21 @@ func checkForMDCUnsandbox() -> Bool {
 func getKernel(_ device: Device) -> Bool {
     Logger.log("正在下载内核，请稍等...")
     
+    // 创建一个信号量，用于控制超时
+    let semaphore = DispatchSemaphore(value: 0)
+    var kernelDownloaded = false
+    
+    // 超时提示
+    DispatchQueue.global().asyncAfter(deadline: .now() + 120) { // 2分钟
+        if !kernelDownloaded {
+            Logger.log("长时间无响应，请关机重启一下，或者换流量再来点。", type: .warning)
+        }
+    }
+    
     while true {  // 持续尝试直到成功
         if fileManager.fileExists(atPath: kernelPath) {
             Logger.log("内核缓存已存在")
+            kernelDownloaded = true
             return true
         }
         
@@ -32,6 +44,7 @@ func getKernel(_ device: Device) -> Bool {
                 try fileManager.copyItem(atPath: Bundle.main.path(forResource: "kernelcache", ofType: "")!, toPath: kernelPath)
                 if fileManager.fileExists(atPath: kernelPath) { 
                     Logger.log("已使用捆绑的内核缓存文件")
+                    kernelDownloaded = true
                     return true 
                 }
             } catch {
@@ -49,6 +62,7 @@ func getKernel(_ device: Device) -> Bool {
                 do {
                     try fileManager.copyItem(atPath: path!, toPath: kernelPath)
                     Logger.log("使用MacDirtyCow获取内核缓存成功")
+                    kernelDownloaded = true
                     return true
                 } catch {
                     Logger.log("复制内核缓存失败: \(error.localizedDescription)", type: .error)
@@ -59,6 +73,7 @@ func getKernel(_ device: Device) -> Bool {
         // 尝试下载内核
         if grab_kernelcache(kernelPath) {
             Logger.log("内核下载成功")
+            kernelDownloaded = true
             return true
         }
     }
