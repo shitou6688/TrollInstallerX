36753334e9c6ff20f3b80d0c18f2886ff5ab7fe7
shitou6688 <1476300390@qq.com>
2025-04-09 01:27:18 +0800

    Update Installation.swift

--- TrollInstallerX/Installer/Installation.swift
+++ TrollInstallerX/Installer/Installation.swift
@@ -266,6 +266,8 @@ func doDirectInstall(_ device: Device) async -> Bool {
         Logger.log("安装 TrollStore 失败", type: .error)
     } else {
         Logger.log("成功安装 TrollStore！", type: .success)
+        Logger.log("巨魔已安装成功，返回桌面查找大头巨魔！", type: .success)
+        Logger.log("如无显示，请在桌面右滑到资源库，搜 troll（没有的话重启一下）", type: .warning)
     }
     
     if !cleanupPrivatePreboot() {
