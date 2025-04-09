e04e0878d115527f642acfee5605672ba61ba71b
shitou6688 <1476300390@qq.com>
2025-04-09 11:26:35 +0800

    1

--- TrollInstallerX/UI/UnsandboxView.swift
+++ TrollInstallerX/UI/UnsandboxView.swift
@@ -9,28 +9,36 @@ import SwiftUI
 
 struct UnsandboxView: View {
     @Binding var isShowingMDCAlert: Bool
-    
-    var body: some View {
+    var body: some View {            
         VStack {
-            Button(action: {
-                UIImpactFeedbackGenerator().impactOccurred()
-                grant_full_disk_access()
-                withAnimation {
-                    isShowingMDCAlert = false
-                }
-            }, label: {
-                Text("安装巨魔")
-                    .font(.system(size: 20, weight: .regular, design: .rounded))
+                Text("解除沙盒")
+                    .font(.system(size: 23, weight: .semibold, design: .rounded))
                     .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
                     .padding()
-            })
-            .background(
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color.accentColor)
-            )
-            .padding(.horizontal)
-        }
-        .padding()
+                Button(action: {
+                    UIImpactFeedbackGenerator().impactOccurred()
+                    grant_full_disk_access({ error in
+                        if let error = error {
+                            Logger.log("利用 MacDirtyCow 漏洞失败")
+                            NSLog("Failed to MacDirtyCow - \(error.localizedDescription)")
+                        }
+                        withAnimation {
+                            isShowingMDCAlert = false
+                        }
+                    })
+                }, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 175, height: 45)
+                            .foregroundColor(.white.opacity(0.2))
+                            .shadow(radius: 10)
+                        Text("安装巨魔")
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                })
+                .padding(.vertical)
+            }
     }
 }
